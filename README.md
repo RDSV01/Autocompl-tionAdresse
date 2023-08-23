ca4db4e2ad45539dee2e1a9951af45e179d8c3fa
RDSV01 <72087745+RDSV01@users.noreply.github.com>
2023-08-23 15:46:06 +0200

    Amelioration du code

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# AutocompletionAdresse
-Système simple en javascript permettant de faire de l'autocomplétion lors de la saisie d'une adresse en utilisant l'id de votre input (Utilisation de la Base Adresse Nationale)
+
+# Autocompletion d'adresse facile
+
+Système d'autocompletion d'adresses facile en JavaScript. Ce système fonctionne grâce à la Base d'adresse Nationale, mise en place gratuitement et mis a jour par les services publiques.
+
+Vous pouvez intégrer ce système facilement et rapidement sur votre site en suivant la documentation ci-desous.
+
+
+## Demo
+
+Insert gif or link to demo
+
+
+## Documentation
+
+### Mettre en place le système d'autocompletion
+
+ 1. Télécharger le fichier AutoComp.js et modifier le nom des id selon votre HTML
+[Fichier AutoComp.js](https://linktodocumentation)
+
+2. Mettre en place jQuery sur son site en mettant le code ci-dessous avant l'ouverture de votre balise <body> et modifier l'appel du fichier AutoComp.js selon son emplacement.
+
+```html
+<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+<link rel="stylesheet" href="https://code.jquery.com/ui/1.7.3/themes/base/jquery-ui.css">
+<script src="scriptAppelApi.js" defer></script>
+```
+
+
+## Important
+
+Il est important de bien faire correspondre les "id" et les "names" de votre html et du javascript.
+
+
+## Authors
+
+[RDSV01](https://www.github.com/RDSV01)
+
+[Site web](https://raphds.fr)
+
--- scriptAppelApi.js
+++ scriptAppelApi.js
@@ -2,10 +2,10 @@ jQuery.noConflict();
 //Code pour adresse de facturation
 //Auto-complétion du champ code postal
 
-    jQuery("#billing_postcode").autocomplete({
+    jQuery("#postcode").autocomplete({
         source: function(request, response) {
             jQuery.ajax({
-                url: "https://api-adresse.data.gouv.fr/search/?q=" + jQuery("input[name='billing_postcode']").val() + '&type=municipality&autocomplete=1',
+                url: "https://api-adresse.data.gouv.fr/search/?q=" + jQuery("input[name='postcode']").val() + '&type=municipality&autocomplete=1',
                 data: {
                     q: request.term
                 },
@@ -23,17 +23,17 @@ jQuery.noConflict();
         },
         // Remplissage de la ville correspondant au code postal
         select: function (event, ui) {
-            jQuery('#billing_city').val(ui.item.city);
+            jQuery('#city').val(ui.item.city);
         },
 
     });
 
 
     //Auto-complétion du champ ville
-    jQuery("#billing_city").autocomplete({
+    jQuery("#city").autocomplete({
         source: function (request, response) {
             jQuery.ajax({
-                url: "https://api-adresse.data.gouv.fr/search/?city=" + jQuery("input[name='billing_city']").val(),
+                url: "https://api-adresse.data.gouv.fr/search/?city=" + jQuery("input[name='city']").val(),
                 data: {
                     q: request.term
                 },
@@ -56,13 +56,13 @@ jQuery.noConflict();
         },
         // Remplissage du code postal correspondant a la ville selectionnée
         select: function (event, ui) {
-            jQuery('#billing_postcode').val(ui.item.postcode);
+            jQuery('#postcode').val(ui.item.postcode);
         }
     });
 
 
     //Auto-complétion du champ adresse
-    jQuery("#billing_address_1").autocomplete({
+    jQuery("#address").autocomplete({
         source: function (request, response) {
             jQuery.ajax({
                 url: "https://api-adresse.data.gouv.fr/search/",
@@ -84,151 +84,7 @@ jQuery.noConflict();
         },
         // Remplissage du code postal et de la ville correspondant a l'adresse selectionnée
         select: function (event, ui) {
-            jQuery('#billing_postcode').val(ui.item.postcode);
-            jQuery('#billing_city').val(ui.item.city);
+            jQuery('#postcode').val(ui.item.postcode);
+            jQuery('#city').val(ui.item.city);
         }
     });
-
-
-
-
-
-//Code pour adresse de livraison
-
-
-
-//Auto-complétion du champ code postal
-jQuery("#shipping_postcode").autocomplete({
-    source: function (request, response) {
-        jQuery.ajax({
-            url: "https://api-adresse.data.gouv.fr/search/?q=" + jQuery("input[name='shipping_postcode']").val() + '&type=municipality&autocomplete=1',
-            data: {
-                q: request.term
-            },
-            dataType: "json",
-            success: function (data) {
-                response(jQuery.map(data.features, function (item) {
-                    return {
-                        label: item.properties.postcode + " – " + item.properties.city,
-                        city: item.properties.city,
-                        value: item.properties.postcode
-                    };
-                }));
-            }
-        });
-    },
-    // Remplissage de la ville correspondant au code postal
-    select: function (event, ui) {
-        jQuery('#shipping_city').val(ui.item.city);
-    },
-
-});
-
-
-//Auto-complétion du champ ville
-jQuery("#shipping_city").autocomplete({
-    source: function (request, response) {
-        jQuery.ajax({
-            url: "https://api-adresse.data.gouv.fr/search/?city=" + jQuery("input[name='shipping_city']").val(),
-            data: {
-                q: request.term
-            },
-            dataType: "json",
-            success: function (data) {
-                var cities = [];
-                response(jQuery.map(data.features, function (item) {
-                    // Ajout d'un tableau pour éviter les doublons
-                    if (jQuery.inArray(item.properties.postcode, cities) == -1) {
-                        cities.push(item.properties.postcode);
-                        return {
-                            label: item.properties.postcode + " - " + item.properties.city,
-                            postcode: item.properties.postcode,
-                            value: item.properties.city
-                        };
-                    }
-                }));
-            }
-        });
-    },
-    // Remplissage du code postal correspondant a la ville selectionnée
-    select: function (event, ui) {
-        jQuery('#shipping_postcode').val(ui.item.postcode);
-    }
-});
-
-
-//Auto-complétion du champ adresse
-jQuery("#shipping_address_1").autocomplete({
-    source: function (request, response) {
-        jQuery.ajax({
-            url: "https://api-adresse.data.gouv.fr/search/",
-            data: {
-                q: request.term
-            },
-            dataType: "json",
-            success: function (data) {
-                response(jQuery.map(data.features, function (item) {
-                    return {
-                        label: item.properties.name + ", " + item.properties.postcode + ", " + item.properties.city,
-                        postcode: item.properties.postcode,
-                        city: item.properties.city,
-                        value: item.properties.name
-                    };
-                }));
-            }
-        });
-    },
-    // Remplissage du code postal et de la ville correspondant a l'adresse selectionnée
-    select: function (event, ui) {
-        jQuery('#shipping_postcode').val(ui.item.postcode);
-        jQuery('#shipping_city').val(ui.item.city);
-    }
-});
-
-
-//Code permettant de supprimer le code postal ou la ville si l'un d'eux est modifié manuellement
-//Permet d'éviter les fausses adresses
-const adresseLivrai = document.getElementById("shipping_address_1");
-const cpLivrai = document.getElementById("shipping_postcode");
-const villeLivrai = document.getElementById("shipping_city");
-
-const listeAdresseLivrai = document.getElementById("ui-id-6");
-const listeCpLivrai = document.getElementById("ui-id-4");
-const listeVilleLivrai = document.getElementById("ui-id-5");
-
-cpLivrai.onclick = function(){ 
-    if((cpLivrai.value != "") || (cpLivrai.value == "")){ 
-        cpLivrai.value = "";
-        villeLivrai.value = "";
-    }
-}
-villeLivrai.onclick = function(){ 
-    if((villeLivrai.value != "") || (villeLivrai.value == "")){ 
-        villeLivrai.value = "";
-        cpLivrai.value = "";
-    }
-}
-
-
-
-const adresseFact = document.getElementById("billing_address_1");
-const cpFact = document.getElementById("billing_postcode");
-const villeFact = document.getElementById("billing_city");
-
-const listeAdresseFact = document.getElementById("ui-id-3");
-const listeCpFact = document.getElementById("ui-id-1");
-const listeVilleFact = document.getElementById("ui-id-2");
-
-
-cpFact.onclick = function(){ 
-    if((cpFact.value != "") || (cpFact.value == "")){ 
-        cpFact.value = "";
-        villeFact.value = "";  
-    }
-}
-villeFact.onclick = function(){ 
-    if((villeFact.value != "") || (villeFact.value == "")){ 
-        villeFact.value = "";
-        cpFact.value = "";
-    }
-}
